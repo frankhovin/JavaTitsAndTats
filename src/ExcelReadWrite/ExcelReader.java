22e305868fb3d53f50b598b242ef5eeb8fd3be5a
Frank H <frankhovin@gmail.com>
2018-05-31 10:35:39 +0200

    Added example code for getting a matched value's adjacent cell's values.

--- src/ExcelReadWrite/ExcelReader.java
+++ src/ExcelReadWrite/ExcelReader.java
@@ -1,6 +1,10 @@
 package ExcelReadWrite;
 
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -12,36 +16,99 @@ import java.io.InputStream;
 import java.util.Iterator;
 
 public class ExcelReader {
-    public static void readXLSXFile (String file) throws IOException {
-        InputStream ExcelFileToRead = new FileInputStream(file);
+    /**
+     * Reads the whole sheet (limited to?) and prints each cell value.
+     *
+     * @param filename
+     * @param sheetname
+     * @throws IOException
+     */
+    public static void readWholeSheet (String filename, String sheetname) throws IOException {
+        InputStream ExcelFileToRead = new FileInputStream(filename);
         XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
 
-        XSSFWorkbook test = new XSSFWorkbook();
-
-        XSSFSheet sheet = wb.getSheetAt(0);
+        XSSFSheet sheet = wb.getSheet(sheetname);
         XSSFRow row;
         XSSFCell cell;
 
         Iterator rows = sheet.rowIterator();
 
         while (rows.hasNext()) {
-            row=(XSSFRow) rows.next();
+            row =(XSSFRow) rows.next();
             Iterator cells = row.cellIterator();
             while (cells.hasNext()) {
                 cell=(XSSFCell) cells.next();
 
-                if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
+                System.out.print(cell.getStringCellValue()+" ");
+
+                // If we need to know what data types are in the cells:
+                // Deprecated:
+                /*if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
                     System.out.print(cell.getStringCellValue()+" ");
                 }
                 else if(cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
                     System.out.print(cell.getNumericCellValue()+" ");
                 } else {
-                    //U Can Handel Boolean, Formula, Errors
-                }
+                }*/
+                // Use getCellTypeEnum() instead (below). The plan is to rename getCellTypeEnum()
+                // back to getCellType() in POI 4.0.
+                /*if (cell.getCellTypeEnum() == CellType.STRING) {
+                    System.out.print(cell.getStringCellValue()+" ");
+                } else if (cell.getCellTypeEnum() == CellType.NUMERIC)) {
+                //    System.out.print(cell.getNumericCellValue()+" ");
+                }*/
+
             }
 
             System.out.println();
         }
-
     }
+
+    /**
+     * Search the sheet for a specific string and get the values in the adjacent cells.
+     *
+     * @param filename
+     * @param sheetname
+     * @param str
+     * @throws IOException
+     */
+    public static String[] getAdjacentCells (String filename, String sheetname, String str) throws IOException {
+        InputStream ExcelFileToRead = new FileInputStream(filename);
+        XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
+
+        XSSFSheet sheet = wb.getSheet(sheetname);
+        XSSFRow row;
+        XSSFCell cell;
+
+        Iterator rows = sheet.rowIterator();
+        String[] values = new String[2];
+
+        while (rows.hasNext()) {
+            row =(XSSFRow) rows.next();
+            Iterator cells = row.cellIterator();
+            while (cells.hasNext()) {
+                cell = (XSSFCell) cells.next();
+                if (cell.getStringCellValue().equals(str)) {
+                    cell = (XSSFCell) cells.next();
+                    values[0] = cell.getStringCellValue();
+                    cell = (XSSFCell) cells.next();
+                    values[1] = cell.getStringCellValue();
+
+                    return values;
+                }
+            }
+
+        }
+
+        return new String[2];
+    }
+
+
+
+
+
+
+
+
+
 }
--- src/Runners.java
+++ src/Runners.java
@@ -6,6 +6,12 @@ import java.io.IOException;
 
 public class Runners {
     public static void main(String args[]) throws IOException {
-        ExcelReader.readXLSXFile("files/excelfile.xlsx");
+        //ExcelReader.readWholeSheet("files/excelfile.xlsx", "Sheet1");
+
+        String info[] = ExcelReader.getAdjacentCells(
+                "files/excelfile.xlsx", "Sheet1", "Firstname Lastname");
+        System.out.print(info[0] + "," + info[1]);
+
+
     }
 }
